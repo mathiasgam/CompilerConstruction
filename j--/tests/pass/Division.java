419f8fe3c32dbf3c8ef0a618fb2708ce8b6936c3
NOTEBOOK-XPS\mathi <mathiasgam@hotmail.com>
2019-02-28 11:45:52 +0100

    initial commit

---
+++ j--/tests/pass/Division.java
@@ -0,0 +1,12 @@
+package pass;
+import java.lang.System;
+
+public class Division {
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
+    //public static void main (String[] args) {
+    //    System.out.println(divide(10, 2));
+    //}
+}
---
+++ j--/tests/pass/HelloWorld2.java
@@ -0,0 +1,11 @@
+package pass;
+
+import java.lang.System;
+
+class HelloWorld2 {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 5;
+        System.out.println("Remainder of: 2 % 5 = " + (a % b));
+    }
+}
