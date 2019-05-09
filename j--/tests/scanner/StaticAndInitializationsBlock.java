1cb49297c8bb020c9fe268605fdccdf21630a451
NOTEBOOK-XPS\mathi <mathiasgam@hotmail.com>
2019-05-09 09:09:02 +0200

    Added block test code

---
+++ j--/tests/scanner/StaticAndInitializationsBlock.java
@@ -0,0 +1,15 @@
+public class Separators {
+
+    static int x;
+    static {
+        x = 0;
+    }
+
+    {
+        x++;
+        y++;
+    }
+
+    public static void main(String[] args) {
+    }
+}
